d8cb05a293806c804c4d13f96272812fa343d821
Andrii Moisol <andrii.moisol@gmail.com>
2023-12-11 11:57:40 +0200

    Initial Commit

---
+++ BattleCity/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BattleCity
+//
+//  Created by Andrii Moisol on 11.12.2023.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
